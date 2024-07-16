1ae6e2a3a10c884ebf352c6dab0135efd0efcc8a
afrondozo <106889542+afrondozo@users.noreply.github.com>
2024-07-16 20:24:00 +0000

    progress

--- Guessordle Game/guessordle/src/App.tsx
+++ Guessordle Game/guessordle/src/App.tsx
@@ -1,35 +1,58 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
 
 
 
 
 function GameBoard() {
-
   type Color = 'green' | 'grey' | 'white';
-
   const [color, setColor] = useState<Color>("white");
  
-  const handleClick =(newColor: Color) => {
-    setColor(newColor);
+  const word = 'DECOY';
+
+  const handleClick =() => {
+    
+    setColor('green');
   }
 
-  useEffect( () => {
 
-  })
 
   return(
     <div className='gameboard'>
-      <div onClick={handleClick('green')}> 1 </div>
+      <div className={color} onClick={handleClick} > {word[0]} </div>
       <div> 2 </div>
       <div> 3 </div>
       <div> 4 </div>
-      <div> 5 </div>
+      <div> u </div>
       <div> 6 </div>
       <div> 7 </div>
       <div> 8 </div>
-      <div> 9 </div>
+      <div> n </div>
+      <div> n </div>
+      <div> n </div>
+      <div> n </div>
+      <div> n </div>
+      <div> n </div>
+      <div> n </div>
+      <div> n </div>
+      <div> n </div>
+      <div> n </div>
+      <div> n </div>
+      <div> n </div>
+      <div> n </div>
+      <div> n </div>
+      <div> n </div>
+      <div> n </div>
+      <div> n </div>
+    </div>
+  );
+}
+
+function Tries() {
+  return(
+    <div>
+      <h1> TRIES: </h1>
     </div>
   );
 }
@@ -41,6 +64,7 @@ function App() {
       <header className='title'> Guessordle </header>
       <div className='container'>
         <GameBoard />
+        <Tries />
       </div>
     </div>
   );
