b6e4834f302029a73d8e047482070d59d0b82b2d
afrondozo <106889542+afrondozo@users.noreply.github.com>
2024-07-23 20:41:25 +0000

    Made changes to the start page

--- Guessordle Game/guessordle/src/components/Start.js
+++ Guessordle Game/guessordle/src/components/Start.js
@@ -3,8 +3,12 @@ function Start({ onStartClick }) {
     return(
         <div className="start-screen">
             <div className="start-screen-container">
-                <h1>Welcome to Your App</h1>
-                <button onClick={onStartClick}>Click to play</button>
+                <h2> Insert Name </h2>
+                <p> 
+                    Flip over cards to uncover the hidden <br/>
+                    5-letter word 
+                </p>
+                <button className='start-btn' onClick={onStartClick}> Play </button>
             </div>
         </div>
     );
