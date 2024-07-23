9c34dd94e46fe04d5cf5b28251cf77fe8aec0708
afrondozo <106889542+afrondozo@users.noreply.github.com>
2024-07-23 16:28:15 +0000

    Made changes to website

--- Website/script.js
+++ Website/script.js
@@ -4,3 +4,27 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+function downloadResume() {
+    var fileURL = 'assets/my-resume.pdf';
+
+    // Create new XMLHttpRequest
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', fileURL, true);
+    xhr.responseType = 'blob'; // Set responseType as blob
+
+    // When the request is successful
+    xhr.onload = function() {
+        // Create a blob object from the response
+        var blob = xhr.response;
+        
+        // Create a link element, set its href attribute, and trigger a click to download
+        var downloadLink = document.createElement('a');
+        downloadLink.href = window.URL.createObjectURL(blob);
+        downloadLink.download = 'my-resume.pdf'; // Specify the filename
+        downloadLink.click();
+    };
+
+    // Send the request
+    xhr.send();
+}
